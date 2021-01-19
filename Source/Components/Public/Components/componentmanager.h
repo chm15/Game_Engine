7a938ac9171ec169a907aa43e0c48814289bb722
Connor McLaughlan <cmclaughlan15@gmail.com>
2021-01-19 19:03:31 +0000

    Initial day commit.

--- Source/Components/Public/Components/componentmanager.h
+++ Source/Components/Public/Components/componentmanager.h
@@ -6,7 +6,10 @@
 
 #pragma once
 
+#include <memory>
+#include <unordered_map>
 #include <Entities/entity.h>
+#include "componentarray.h"
 
 class ComponentManager {
 public:
@@ -30,6 +33,13 @@ public:
 
 public:
     ComponentManager() = default;
+
+    // Maps from classID to ComponentArray
+    std::unordered_map<int, std::shared_ptr<ComponentArray> > componentArrays;
+    // TODO: COMPONENTARRAY NEEDS A SUPERCLASS THAT ISN'T TEMPLATED!
+
+    // Helper function for getting component array.
+    //std::shared_ptr<ComponentArray> getComponentArray(int classID) {};
 
 };
 
